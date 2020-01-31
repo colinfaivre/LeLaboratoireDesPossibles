e0b8b477bc5485b3c09e6fd4d6d831f2b0184adc
Colin Faivre <colin.faivre@gmail.com>
2020-01-31 09:39:19 +0100

    fix css

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,35 @@
 <template>
     <v-container
+        fill-height
+        fluid
     >
-
         <v-row
+            class="mt-6"
             no-gutters
-        >
-            <v-col class="text-center">
-                <img src="@/assets/images/labo-logo.svg" alt="Le laboratoire des possibles">
-            </v-col>
-        </v-row>
-
-        <v-row
-            no-gutters
-            justify="center"
-            class="text-center"
-        >
-            <v-col>
-                <h1 class="brand secondary--text">
-                    Le laboratoire
-                    <br/>
-                    des possibles
-                </h1>
-            </v-col>
-        </v-row>
-
-        <v-row
-            no-gutters
+            align="center"
             justify="center"
         >
-            <div class="d-flex align-center">
-                <img src="@/assets/images/vue-logo.svg" alt="Vue.js" class="mr-3">
+            <div
+                class="d-flex flex-column align-center justify-center"
+            >
+                    <img src="@/assets/images/labo-logo.svg" alt="Le laboratoire des possibles">
+                    <h1 class="brand secondary--text">
+                        Le laboratoire
+                        <br/>
+                        des possibles
+                    </h1>
+                    <div class="d-flex align-center">
+                        <img src="@/assets/images/vue-logo.svg" alt="Vue.js" class="mr-3">
 
-                <p class="text-left grey--text body-1 mt-4">
-                    Votre application web
-                    <br>
-                    développée en Vue.js
-                </p>
+                        <p class="text-left grey--text body-1 mt-4">
+                            Votre application web
+                            <br>
+                            développée en Vue.js
+                        </p>
+                    </div>
             </div>
         </v-row>
-        
+
         <v-row
             no-gutters
             align="center"
@@ -71,6 +62,5 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
 </style>
