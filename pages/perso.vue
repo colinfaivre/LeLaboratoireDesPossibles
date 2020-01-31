7578915aa5b5de71eb0d663b9430522a85ae59b6
Colin Faivre <colin.faivre@gmail.com>
2020-01-31 17:34:38 +0100

    Add perso projects

--- pages/perso.vue
+++ pages/perso.vue
@@ -120,6 +120,52 @@
           <img :src="require('@/assets/images/tech/pwa.svg')" alt="progressive web app" height="30px" class="mr-2">
         </v-card-actions>
       </v-card>
+
+      <v-card
+        class="ma-2"
+        width="450"
+        min-width="320"
+      >
+        <v-img
+          class="white--text align-end"
+          height="250px"
+          :src="require('@/assets/images/perso/elevator.png')"
+        >
+        </v-img>
+        <v-card-title color="primary">Simulation d'ascenceurs</v-card-title>
+
+        <v-card-text>
+          <div class="text--primary">Un clone de Slack</div>
+          <div class="text--grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        </v-card-text>
+
+        <v-card-actions class="ml-2 mb-2">
+          <img :src="require('@/assets/images/tech/javascript.png')" alt="javascript" height="30px" class="mr-2">
+        </v-card-actions>
+      </v-card>
+
+      <v-card
+        class="ma-2"
+        width="450"
+        min-width="320"
+      >
+        <v-img
+          class="white--text align-end"
+          height="250px"
+          :src="require('@/assets/images/perso/tictactoe.png')"
+        >
+        </v-img>
+        <v-card-title color="primary">Vanilla JS TicTacToe</v-card-title>
+
+        <v-card-text>
+          <div class="text--primary">Un clone de Slack</div>
+          <div class="text--grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        </v-card-text>
+
+        <v-card-actions class="ml-2 mb-2">
+          <img :src="require('@/assets/images/tech/javascript.png')" alt="javascript" height="30px" class="mr-2">
+        </v-card-actions>
+      </v-card>
     </v-row>
   </v-container>
 </template>
