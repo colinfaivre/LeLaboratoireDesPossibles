569dd7c8c2d46f19371800c5b6abec2decfe026a
Colin Faivre <colin.faivre@gmail.com>
2020-02-06 16:40:50 +0100

    Create ProjectCards component and refactor existing pages using it

--- pages/ar.vue
+++ pages/ar.vue
@@ -1,41 +1,17 @@
 <template>
   <v-container fluid>
-    <v-row justify="center">
 
-      <v-card
-        v-for="project in arProjects" :key="project.index"
-        class="ma-2"
-        width="450"
-        min-width="320"
-      >
-        <v-img
-          class="white--text align-end"
-          height="250px"
-          :src="require('@/assets/images/screenshots/' + project.screenshot )"
-        >
-        </v-img>
-        <v-card-title color="primary">
-          {{ project.title }}
-        </v-card-title>
-        <v-card-text>
-          <div class="text--primary">
-            {{ project.subtitle }}
-          </div>
-          <div class="text--grey">
-            {{ project.text }}
-          </div>
-        </v-card-text>
-        <v-card-actions class="ml-2 mb-2">
-          <img  v-for="techno in project.technos" :key="techno.index" :src="require('@/assets/images/tech/' + techno + '.png')" :alt="techno" height="30px" class="mr-2">
-        </v-card-actions>
-      </v-card>
+    <ProjectCards :projects="arProjects"/>
 
-    </v-row>
   </v-container>
 </template>
 
 <script>
+import ProjectCards from '@/components/ProjectCards'
 export default {
+  components: {
+    ProjectCards,
+  },
   computed: {
     arProjects() {
       return this.$store.getters['projects/arProjects']
